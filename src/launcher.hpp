b487164c55bcb6052b3bbdd53c9e5d4afcc92a04
akashche <akashche@redhat.com>
2017-10-11 07:38:18 -0700

    add missed header

---
+++ src/launcher.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 akashche at redhat.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ITW_LAUNCHER_HPP
+#define ITW_LAUNCHER_HPP
+
+#define IDS_VENDOR_DIRNAME 141
+#define IDS_APP_DIRNAME 142
+#define IDS_ERROR_DIALOG_TITLE 143
+#define IDS_BROWSER_ERROR_TEXT 144
+#define IDS_ERROR_HELP_URL 145
+#define IDS_ERROR_DIALOG_HEADER 146
+#define IDS_ERROR_DIALOG_SUBHEADER 147
+#define IDS_JVM_OPTIONS 148
+#define IDS_NO_ARGS_ERROR_MESSAGE 149
+
+#endif // ITW_LAUNCHER_HPP
